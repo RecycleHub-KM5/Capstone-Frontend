d7c6d79518026e81f1d25f2f20c0ff1f585f91f5
MaulanaYusuf20 <maulanayusuf200101@gmail.com>
2023-11-16 12:26:25 +0700

    setup rect-router-dom

---
+++ src/assets/pages/LandingPage/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MainLayout from "../../layout/MainLayout";
+
+const LandingPage = () => {
+    return (
+        <MainLayout>
+            <div className="container">
+                <h1 className="">LandingPage</h1>
+            </div>
+        </MainLayout>
+    );
+};
+
+export default LandingPage;
