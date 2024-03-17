a205fdfadf986ffbca6b1df1b2e59405d37cda33
MaulanaYusuf20 <maulanayusuf200101@gmail.com>
2024-03-17 17:11:25 +0700

    update

--- src/components/GlobalComponent/Navbar/index.jsx
+++ src/components/GlobalComponent/Navbar/index.jsx
@@ -11,56 +11,33 @@ const Navbar = () => {
             <div className="container-fluid p-0 my-auto">
                 <Link className="navbar-brand color-light poppins-bold" to="/">
                     <div className="wrapper d-flex justify-content-center align-items-center">
-                        <img
-                            src="https://cdn.discordapp.com/attachments/1164510335114428526/1179740777547579392/image.png?ex=657ae237&is=65686d37&hm=0f8279fbb3e6350a497862e02719333f91030ec351bef2726cd2021972d774f2&"
-                            className="brand-image"
-                        />
+                        <img src="/icons/logo.svg" className="brand-image" />
                         <h1 className="font-paragraph m-0 ms-1">RecycleHub</h1>
                     </div>
                 </Link>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <img src="/icons/bar.svg" />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <div className="wrapper">
                         <ul className="navbar-nav ">
                             <li className="nav-item">
-                                <NavLink
-                                    to="/"
-                                    className="navlink ms-4 poppins-medium"
-                                >
+                                <NavLink to="/" className="navlink ms-4 poppins-medium">
                                     Home
                                 </NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink
-                                    to="/gallery"
-                                    className="navlink ms-4 poppins-medium"
-                                >
+                                <NavLink to="/gallery" className="navlink ms-4 poppins-medium">
                                     Galeri
                                 </NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink
-                                    to="/information"
-                                    className="navlink ms-4 poppins-medium"
-                                >
+                                <NavLink to="/information" className="navlink ms-4 poppins-medium">
                                     Informasi
                                 </NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink
-                                    to={status ? "/products" : "/login"}
-                                    className="navlink ms-4 me-5 poppins-medium"
-                                >
+                                <NavLink to={status ? "/products" : "/login"} className="navlink ms-4 me-5 poppins-medium">
                                     Produk
                                 </NavLink>
                             </li>
@@ -68,39 +45,19 @@ const Navbar = () => {
                     </div>
                     {status == null && (
                         <div className="wrapper ms-3" id="btn-wrapper-auth">
-                            <NavLink
-                                to="/login"
-                                id="btn-login"
-                                className="navlink poppins-semiBold"
-                            >
+                            <NavLink to="/login" id="btn-login" className="navlink poppins-semiBold">
                                 Masuk
                             </NavLink>
-                            <Link
-                                id="btn-register"
-                                className="poppins-semiBold"
-                                to="register"
-                            >
+                            <Link id="btn-register" className="poppins-semiBold" to="register">
                                 Daftar
                             </Link>
                         </div>
                     )}
                     {status && (
-                        <div
-                            className="wrapper btn-wrapper-none ms-3"
-                            id="btn-wrapper-user"
-                        >
-                            <Link
-                                id="btn-user"
-                                to="/users/transaction"
-                                className="text-decoration-none"
-                            >
-                                <h3 className="poppins-medium color-light font-paragraph mt-2">
-                                    {userName}
-                                </h3>
-                                <img
-                                    src="https://i.ibb.co/gZyqXvV/dummy-ava.png"
-                                    alt=""
-                                />
+                        <div className="wrapper btn-wrapper-none ms-3" id="btn-wrapper-user">
+                            <Link id="btn-user" to="/users/transaction" className="text-decoration-none">
+                                <h3 className="poppins-medium color-light font-paragraph mt-2">{userName}</h3>
+                                <img src="https://i.ibb.co/gZyqXvV/dummy-ava.png" alt="" />
                             </Link>
                         </div>
                     )}
--- src/components/Information/swiper.jsx
+++ src/components/Information/swiper.jsx
@@ -2,81 +2,52 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
 const InformationSwiper = () => {
-  return (
-    <div className="col-12">
-      <Swiper className="mySwiper" slidesPerView={3} spaceBetween={30}>
-        <SwiperSlide>
-          <div class="card-container d-flex flex-column">
-            <div class="card-head">
-              <img src="https://cdn.discordapp.com/attachments/1164510335114428526/1164510421210890310/image.png?ex=654379d7&is=653104d7&hm=c9be6202856e2d016f844865e81c9fd59d921aa0525b482979c91a1b06f79eda&" />
-            </div>
-            <div class="card-body">
-              <p class="font-paragraph lh-base mt-3 poppins-regular color-light">
-                Sampah yang tidak dikelola dengan baik dapat memiliki dampak
-                negatif yang serius terhadap lingkungan, kesehatan manusia, dan
-                masyarakat secara umum, Sampah yang dibiarkan berserakan atau
-                dibuang sembarangan dapat mencemari tanah, air, dan udara.
-                Produk-produk kimia berbahaya dalam sampah dapat merembes ke
-                dalam tanah dan air, mengkontaminasi sumber daya alam dan
-                memengaruhi kehidupan hewan serta tumbuhan.
-              </p>
-            </div>
-          </div>
-          </SwiperSlide>
-          <SwiperSlide>
-          <div class="card-container d-flex flex-column">
-            <div class="card-head">
-              <img src="https://cdn.discordapp.com/attachments/1164510335114428526/1164513037626122280/image.png?ex=65437c47&is=65310747&hm=1ffe8a3c8b7000bbf0611529d282a48add9d7b36c4c83fcccccde27c8c5d048c&" />
-            </div>
-            <div class="card-body">
-              <p class="font-paragraph lh-base mt-3 poppins-regular color-light">
-                Sampah yang berserakan menciptakan pemandangan yang tidak
-                menarik dan kotor. Ini bisa mencakup tumpukan sampah di tepi
-                jalan, pantai, taman, atau daerah wisata lainnya. Kehadiran
-                sampah yang mencolok dapat mengganggu pengalaman wisatawan yang
-                mencari lingkungan yang bersih, indah, dan rapi, jika berupa
-                sampah tajam atau bahan berbahaya, dapat menjadi bahaya bagi
-                orang-orang yang mengunjungi tempat tersebut.
-              </p>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div class="card-container d-flex flex-column">
-            <div class="card-head">
-              <img src="https://cdn.discordapp.com/attachments/1164510335114428526/1164513456725164053/image.png?ex=65437cab&is=653107ab&hm=ae5a67bf5607562cc1378862d604009b1254be540459dd7578c160af4c8f7257&" />
-            </div>
-            <div class="card-body">
-              <p class="font-paragraph lh-base mt-3 poppins-regular color-light">
-                CO2 adalah gas rumah kaca utama yang dihasilkan dari pembakaran
-                sampah. Ketika bahan organik seperti plastik, kertas, dan limbah
-                makanan terbakar, mereka melepaskan karbon dioksida ke atmosfer.
-                Karbon dioksida adalah penyebab utama perubahan iklim dan
-                pemanasan gl.obal., yang kedua adalah Metana yang merupakan gas
-                rumah kaca yang lebih kuat daripada karbon dioksida dalam
-                menangkap panas dari matahari.
-              </p>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div class="card-container d-flex flex-column">
-            <div class="card-head">
-              <img src="https://cdn.discordapp.com/attachments/1164510335114428526/1164516415282348052/image.png?ex=65437f6d&is=65310a6d&hm=1a8a98cff4c323efd39c4ae4a841a989abc11778b89c11b014503f49d5e487a3&" />
-            </div>
-            <div class="card-body">
-              <p class="font-paragraph lh-base mt-3 poppins-regular color-light">
-                Selanjutnya hasil uang yang diperoleh dari penjualan barang daur
-                ulang akan kami sumbangkan pada Panti Asuhan & Orang yang
-                membutuhkan, proses pemberian hasil ini juga bisa anda lihat
-                lebih lengkap di menu "informasi".
-              </p>
-            </div>
-          </div>
-        </SwiperSlide>
-      </Swiper>
-    </div>
-  );
+    return (
+        <div className="col-12">
+            <Swiper className="mySwiper" slidesPerView={3} spaceBetween={30}>
+                <SwiperSlide>
+                    <div class="card-container d-flex flex-column">
+                        <div class="card-head">
+                            <img src="https://cdn.discordapp.com/attachments/1164510335114428526/1164510421210890310/image.png?ex=66054657&is=65f2d157&hm=4449fd1515e93a8bfed2f442624be3bbc194ab735e7d762546d74c3f53eb9b9d&" />
+                        </div>
+                        <div class="card-body">
+                            <p class="font-paragraph lh-base mt-3 poppins-regular color-light">Sampah yang tidak dikelola dengan baik dapat memiliki dampak negatif yang serius terhadap lingkungan, kesehatan manusia, dan masyarakat secara umum, Sampah yang dibiarkan berserakan atau dibuang sembarangan dapat mencemari tanah, air, dan udara. Produk-produk kimia berbahaya dalam sampah dapat merembes ke dalam tanah dan air, mengkontaminasi sumber daya alam dan memengaruhi kehidupan hewan serta tumbuhan.</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div class="card-container d-flex flex-column">
+                        <div class="card-head">
+                            <img src="https://cdn.discordapp.com/attachments/1164510335114428526/1164513037626122280/image.png?ex=660548c7&is=65f2d3c7&hm=141742fc4da4ccc67ee6d73e5944d13bcd5f4a3042ebecd301486133e30d23b8&" />
+                        </div>
+                        <div class="card-body">
+                            <p class="font-paragraph lh-base mt-3 poppins-regular color-light">Sampah yang berserakan menciptakan pemandangan yang tidak menarik dan kotor. Ini bisa mencakup tumpukan sampah di tepi jalan, pantai, taman, atau daerah wisata lainnya. Kehadiran sampah yang mencolok dapat mengganggu pengalaman wisatawan yang mencari lingkungan yang bersih, indah, dan rapi, jika berupa sampah tajam atau bahan berbahaya, dapat menjadi bahaya bagi orang-orang yang mengunjungi tempat tersebut.</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div class="card-container d-flex flex-column">
+                        <div class="card-head">
+                            <img src="https://cdn.discordapp.com/attachments/1164510335114428526/1164513456725164053/image.png?ex=6605492b&is=65f2d42b&hm=4c2d3f4960abb475b86262b73fb9cfe6b94c35e5540c9f733cb3c07ab2d34e25&" />
+                        </div>
+                        <div class="card-body">
+                            <p class="font-paragraph lh-base mt-3 poppins-regular color-light">CO2 adalah gas rumah kaca utama yang dihasilkan dari pembakaran sampah. Ketika bahan organik seperti plastik, kertas, dan limbah makanan terbakar, mereka melepaskan karbon dioksida ke atmosfer. Karbon dioksida adalah penyebab utama perubahan iklim dan pemanasan gl.obal., yang kedua adalah Metana yang merupakan gas rumah kaca yang lebih kuat daripada karbon dioksida dalam menangkap panas dari matahari.</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div class="card-container d-flex flex-column">
+                        <div class="card-head">
+                            <img src="https://cdn.discordapp.com/attachments/1164510335114428526/1164516415282348052/image.png?ex=66054bed&is=65f2d6ed&hm=0487c84844609d9bf4f1f6d5ae5289eacdcfb54c86bee51870611028624ab522&" />
+                        </div>
+                        <div class="card-body">
+                            <p class="font-paragraph lh-base mt-3 poppins-regular color-light">Selanjutnya hasil uang yang diperoleh dari penjualan barang daur ulang akan kami sumbangkan pada Panti Asuhan & Orang yang membutuhkan, proses pemberian hasil ini juga bisa anda lihat lebih lengkap di menu "informasi".</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    );
 };
 
 export default InformationSwiper;
